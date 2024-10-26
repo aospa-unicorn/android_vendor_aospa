d3f8a86b2837757ade1908fac54b919101311099
hiper25 <2243168976@qq.com>
2024-10-27 03:51:35 +0800

    aospa: Build Pixel Launcher
    Change-Id: I87bcaff294ace3455c67706b4d1ac8462436e82c

--- target/product/aospa-target.mk
+++ target/product/aospa-target.mk
@@ -19,6 +19,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Some CTS test case failed after enabling feature config_stopSystemPackagesByDefault
 PRODUCT_PACKAGES += initial-package-stopped-states-aosp.xml
 
+# Google Pixel Launcher
+PRODUCT_PACKAGES += \
+    NexusLauncherRelease
+
+# Google Pixel Launcher Overlay
+PRODUCT_PACKAGES += \
+    NexusLauncherOverlay \
+    PixelLauncherConfigOverlay
+
 # Abstruct
 PRODUCT_PACKAGES += \
     Abstruct
