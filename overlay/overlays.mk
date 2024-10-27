e2f8b4b1de3d910f0c46952fd34f4a3a4d82fc7f
Richard Raya <rdxzv.dev@gmail.com>
2024-10-27 03:32:54 -0300

    fonts: Import hOS font
    Change-Id: I8ae0955b0f94c54ac33aafdac70d7e3ed10a53c0
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- overlay/overlays.mk
+++ overlay/overlays.mk
@@ -38,6 +38,7 @@ PRODUCT_PACKAGES += \
     FontPingFangOverlay \
     FontQuicksandOverlay \
     FontSwitzerOverlay \
+    FontTwainsixevenOverlay \
     FontUrbanistOverlay \
     FontAlbertSansOverlay \
     FontDMSansOverlay \
